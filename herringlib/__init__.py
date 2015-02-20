720712e5f4eac5a7f7c55f88604b18a7bb352a7c
royw <roy@wright.org>
2015-02-20 13:46:34 -0600

    using user herringlib

--- herringlib/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-"""
-The herringlib contains tasks and support functions for building the application.
-These are automatically imported when your herringfile is loaded.
-"""
-
-__version__ = '0.0.1'
